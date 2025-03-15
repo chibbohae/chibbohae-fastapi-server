96bb7d17eb63ae42d5f8a7228b90bd39ff20d9ea
ban_chan <kimmc3423@naver.com>
2025-03-15 21:48:16 +0900

    https를 위한 health_check_router 추가

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from fastapi import FastAPI
-from app.routers import (
-    call_manager_routers,
-    review_routers,
-    call_record_routers,
-)
+from app.routers import call_manager_routers, review_routers, health_check_routers
 from app.services import signaling
 from app.dependencies.db import engine, Base
 from fastapi.middleware.cors import CORSMiddleware
@@ -16,6 +12,7 @@ app = FastAPI()
 app.include_router(review_routers.router)
 app.include_router(call_manager_routers.router)
 app.include_router(signaling.router)
+app.include_router(health_check_routers.router)
 # app.include_router(call_record_routers.router)
 
 
