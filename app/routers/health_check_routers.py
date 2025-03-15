aa2e3e079d889fe937edc1793d352f4ab36e1a35
ban_chan <kimmc3423@naver.com>
2025-03-15 21:57:12 +0900

    루트경로 endpoint 추가

--- app/routers/health_check_routers.py
+++ app/routers/health_check_routers.py
@@ -3,6 +3,11 @@ from fastapi import APIRouter
 router = APIRouter()
 
 
+@router.get("/")
+async def root():
+    return {"message": "Welcome to Chiboohae!"}
+
+
 @router.get("/health", status_code=200)
 async def health_check():
     return {"message": "Success Health Check"}
