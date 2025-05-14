db263db3c555f3821fab8f015c7cc9ae11f0bdcc
KIM 민찬 <163628956+banchan01@users.noreply.github.com>
2025-05-14 19:34:10 +0900

    통화종료에서 call_id redis에서 가져오게함

--- app/services/signaling.py
+++ app/services/signaling.py
@@ -88,7 +88,7 @@ async def websocket_endpoint(websocket: WebSocket, user_id: str):
             # 통화 종료 (Call End)
             elif message_type == "call_end":
 
-                call_id = data.get("call_id")
+                call_id = redis_client.get(f"accept:{caller_id}:{receiver_id}")
                 caller_id = data.get("caller_id")
                 receiver_id = data.get("receiver_id")
 
